09eb5719774dcbd87f1dd6a88e0f399195428b01
thebible75 <incoad@gmail.com>
2019-10-28 12:24:11 +0900

    first commit

--- app/src/main/java/com/example/test_java/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/test_java/MyFirebaseMessagingService.java
@@ -18,12 +18,7 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseMsgService";
 
-    /**
-     * Called when message is received.
-     *
-     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
-     */
-    // [START receive_message]
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
